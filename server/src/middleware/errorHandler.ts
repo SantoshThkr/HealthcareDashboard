583ab820ed9d13aa70065b31c3321080e11e6fcf
SantoshThkr <santosh.thakur36911@gmail.com>
2022-11-20 14:30:00 +0530

    handle api validation and errors

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,16 +1,34 @@
 import { ErrorRequestHandler, RequestHandler } from 'express';
-import { ForeignKeyConstraintError, UniqueConstraintError } from 'sequelize';
+import {
+  DatabaseError,
+  ForeignKeyConstraintError,
+  UniqueConstraintError,
+  ValidationError,
+} from 'sequelize';
 
-import { ApiError } from '../utils/ApiError';
+import { ApiError, FieldErrors } from '../utils/ApiError';
 import { logger } from '../utils/logger';
 
 export const notFoundHandler: RequestHandler = (_req, res) => {
   res.status(404).json({ success: false, message: 'Route not found' });
 };
 
+function fieldErrors(err: ValidationError, message: (path: string) => string): FieldErrors {
+  const errors: FieldErrors = {};
+  err.errors.forEach((item) => {
+    if (item.path) {
+      errors[item.path] = message(item.path);
+    }
+  });
+  return errors;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
   if (err instanceof ApiError) {
+    if (err.status >= 500) {
+      logger.error(`${req.method} ${req.path} failed: ${err.message}`);
+    }
     return res.status(err.status).json({
       success: false,
       message: err.message,
@@ -19,13 +37,19 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
   }
 
   if (err instanceof UniqueConstraintError) {
-    const errors: Record<string, string> = {};
-    err.errors.forEach((item) => {
-      if (item.path) {
-        errors[item.path] = `This ${item.path} is already in use`;
-      }
+    return res.status(409).json({
+      success: false,
+      message: 'Record already exists',
+      errors: fieldErrors(err, (path) => `This ${path} is already in use`),
     });
-    return res.status(409).json({ success: false, message: 'Record already exists', errors });
+  }
+
+  if (err instanceof ValidationError) {
+    return res.status(400).json({
+      success: false,
+      message: 'Validation failed',
+      errors: fieldErrors(err, (path) => `Invalid ${path}`),
+    });
   }
 
   if (err instanceof ForeignKeyConstraintError) {
@@ -35,8 +59,16 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
     });
   }
 
+  if (err instanceof DatabaseError && /invalid input/i.test(err.message)) {
+    return res.status(400).json({ success: false, message: 'Invalid request data' });
+  }
+
   if (err.type === 'entity.parse.failed') {
     return res.status(400).json({ success: false, message: 'Malformed JSON body' });
+  }
+
+  if (err.type === 'entity.too.large') {
+    return res.status(413).json({ success: false, message: 'Request body is too large' });
   }
 
   logger.error(`${req.method} ${req.path} failed: ${err.stack || err}`);
